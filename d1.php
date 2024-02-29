e48fa225107ee8f24dd05209c977c48224d5208a
almeida-cma <93884913+almeida-cma@users.noreply.github.com>
2024-02-29 07:42:38 -0300

    Update d1.php

--- d1.php
+++ d1.php
@@ -13,19 +13,7 @@
     <input type="submit" name="submit" value="Enviar">
 </form>
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST['nome'];
-    $idade = $_POST['idade'];
-
-    // Verifica se o nome e a idade foram fornecidos
-    if (!empty($nome) && !empty($idade)) {
-        echo "<h2>Olá, $nome! Você tem $idade anos.</h2>";
-    } else {
-        echo "<h2>Por favor, informe seu nome e idade.</h2>";
-    }
-}
-?>
+//código php aqui
 
 </body>
 </html>
